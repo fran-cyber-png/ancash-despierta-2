54fc23a44b576f5888245ddbc275564507c69bad
desarrollo06 <desarrollo06@goberna.pe>
2026-08-18 12:57:38 -0500

    Corrige el ícono de "Inicio" para que coincida con Figma (nodo 21:50)
    Era el arrow_back de Material (cabeza compacta), pero el diseño usa una flecha
    larga de línea recta con punta triangular, más ancha que alta.

--- src/sections/articulo/Articulo.tsx
+++ src/sections/articulo/Articulo.tsx
@@ -19,10 +19,10 @@ export function Articulo({ noticia }: { noticia: Noticia }) {
             >
               <svg
                 aria-hidden="true"
-                viewBox="0 0 24 24"
+                viewBox="0 0 448 512"
                 className="h-[28px] w-[28px] shrink-0 fill-current"
               >
-                <path d="M20 11H7.83l4.58-4.59L11 5l-7 7 7 7 1.41-1.41L7.83 13H20z" />
+                <path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z" />
               </svg>
               Inicio
             </Link>
